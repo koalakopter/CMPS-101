3d8cd0fc87966b327bf78cb42e748bc4d24acb70
Julian To <juliantomato123@outlook.com>
2019-04-13 16:22:22 -0700

    starting on LEx now

---
+++ pa1/Lex.java
@@ -0,0 +1,12 @@
+/* JULIAN TO
+ * PA 1
+ * CS 101
+ * PROFESSOR TANTALO
+ * SPRING 2019
+ */
+public class Lex {
+	 public static void main(String[] args){
+		 
+		System.out.println("begin"); 
+	 }
+}
