898245e245f010b5c425476475723c5fe06193cd
Julian To <juliantomato123@outlook.com>
2019-04-14 16:31:55 -0700

    Lex.java all working now

--- pa1/Lex.java
+++ pa1/Lex.java
@@ -27,24 +27,63 @@ public class Lex {
 		Scanner in = new Scanner(new File(args[0]));
 		PrintWriter out = new PrintWriter(new FileWriter(args[1]));
 
+		// counts number of lines in the input file
+		int counter = 0;
 		while (in.hasNextLine()) {
-			// lineNumber++; //what does this even do
-
-			// trim leading and trailing spaces, then add one trailing space so
-			// split works on blank lines
-			String line = in.nextLine().trim() + " ";
-
-			// split line around white space
-			// String[] token = line.split("\\s+");
-			String[] token = line.split("\\r?\\n\"");
-
-			int n = token.length;
-
-			// prints out tokens for testing
-			for (int i = 0; i < n; i++) {
-				System.out.println(token[i]);
-			}
-
+			counter++;
+			in.nextLine();
 		}
+
+		in.close();
+		in = null;
+		String[] input = new String[counter];
+		// open file again
+		in = new Scanner(new File(args[0]));
+
+		int koala = 0;
+		while (in.hasNextLine()) {
+			input[koala] = in.nextLine();
+			koala++;
+		}
+		//System.out.println(counter + " strings read");
+		for (int x = 0; x < counter; x++) {
+			//System.out.println(input[x]);
+		}
+
+		// insertion sort
+		List poi = new List();
+		//start the list 
+		poi.append(0);
+		int i, j = 0;
+		for (i = 1; i < counter; i++) {
+			j = i - 1;
+			String temp = input[i];
+			poi.moveBack();
+			//move the element back until it is lexographically less than the one in front
+			while (j >= 0 && temp.compareTo(input[poi.get()]) <= 0) {
+				poi.movePrev();
+				j--;
+			}
+			if (poi.index() >= 0) {
+				poi.insertAfter(i);
+			} else {
+				poi.prepend(i);
+			}
+		}
+		poi.moveFront();
+		//System.out.println(poi);
+		//System.out.println(input[poi.get()]);
+		//System.out.println(poi.length());
+		
+		//write output to file
+		for(int f = 0; f < poi.length(); f++)
+		{
+			//System.out.println(input[poi.get()]);
+			out.println(input[poi.get()]);
+			poi.moveNext();
+		}
+		// close files
+		in.close();
+		out.close();
 	}
 }
