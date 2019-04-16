b75f1f3dd6b7a9ca4fb174bb68e42a0c99e8c29e
Julian To <juliantomato123@outlook.com>
2019-04-16 16:25:13 -0700

    added 2 new functions

--- pa2/List.c
+++ pa2/List.c
@@ -6,32 +6,71 @@ Spring 2019
 */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "List.h"
 
-//defines the Node which holds all data
+// defines the Node which holds all data
 typedef struct NodeItem {
-    struct NodeItem *nextItem;
-    struct NodeItem *prevItem;
-    int data;
+  struct NodeItem *nextItem;
+  struct NodeItem *prevItem;
+  int data;
 } NodeItem;
 
-//defines the List which holds all the Nodes
+// defines the List which holds all the Nodes
 struct List {
-    NodeItem *head; //defines the head of the list
-    NodeItem *tail; //defines the tail of the list
-    NodeItem *cursor; //defines the cursor object
+  NodeItem *head;   // defines the head of the list
+  NodeItem *tail;   // defines the tail of the list
+  NodeItem *cursor; // defines the cursor object
 };
 
-//makes a new list
-List *newList(void)
-{
-    List *new_List = malloc(sizeof(List));
-    //set head, tail, and cursor all to null
-    new_List->head = NULL;
-    new_List->tail = NULL;
-    new_List->cursor = NULL;
-    return new_List;
+// makes a new list
+List *newList(void) {
+  List *new_List = malloc(sizeof(List));
+  // set head, tail, and cursor all to null
+  new_List->head = NULL;
+  new_List->tail = NULL;
+  new_List->cursor = NULL;
+  return new_List;
+}
+
+// MOST FUNCTIONS TAKE IN THE LIST AS AN ARGUMENT FOR THE FUNCTION
+
+// returns the length of the list, returns zero if empty
+int length(List *L) {
+  NodeItem *pointer = L->head;
+  int length = 0;
+  // if head is null, list is probably empty (barring weird circumstance)
+  if (pointer == NULL) {
+    return length;
+  }
+  length++;
+  while (pointer->nextItem != NULL) {
+    pointer = pointer->nextItem;
+    length++;
+  }
+  return length;
+}
+
+// returns the index (position) of the cursor element
+int index(List *L) {
+  int cursorTracker = -1;
+  // if cursor undefined, return -1
+  NodeItem *cursorRadar = L->cursor;
+  if (cursorRadar == NULL) {
+    return cursorTracker;
+  }
+
+  // otherwise, traverse the list until cursor is located
+  NodeItem *pointer = L->head;
+  while (pointer->nextItem != NULL) {
+    if (pointer == cursorRadar) {
+      break;
+    }
+    pointer = pointer->nextItem;
+    cursorTracker++;
+  }
+  cursorTracker++;
+  return cursorTracker;
 }
